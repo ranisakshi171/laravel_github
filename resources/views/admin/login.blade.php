c1cee224341eda90538f8f057ee7bd58f44a889b
simplilearns <sakshirani181@gmail.com>
2026-06-06 15:46:39 +0530

    laravel upload

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="{{ asset('frontend/css/normalize.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/main.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('frontend/css/font-awesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/flaticon.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
     <style>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/main.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/bootstrap.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/animate.min.css') }}">
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/font-awesome.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('frontend/css/flaticon.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('frontend/vendor/owlcarousel/owl.carousel.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('frontend/vendor/owlcarousel/owl.theme.default.min.css') }}">
